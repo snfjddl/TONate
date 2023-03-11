9a76b8c831aa3418f21ef7b9328a7a3276284293
snfjddl <seokil1220@gmail.com>
2023-03-11 20:50:35 +0900

    refactor: 빌드 결과 폴더 수정

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,4 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
   base: ((process.env.GITHUB_REPOSITORY ?? "") + "/").match(/(\/.*)/)?.[1],
-  build: {
-    outDir: "docs",
-  },
 });
